beb83b1069463d24b0fe8ff546c446028ffead81
WalterFox <john.cobacango@epn.edu.ec>
2024-06-14 12:09:45 -0500

    Creacion de objetos

--- Empresa de transportes/src/Main.java
+++ Empresa de transportes/src/Main.java
@@ -4,6 +4,9 @@ import Rutas.VIP;
 import Rutas.Normal;
 public class Main {
     public static void main(String[] args) {
-
+        Ruta ruta1 = new Ruta("Quito_Guayaquil", 20);
+        Ruta ruta2 = new Ruta("Quito-Tulcán", 17.50);
+        Ruta ruta3 = new Ruta("Quito-Puyo", 15);
+        Ruta ruta4 = new Ruta("Quito-Riobamba", 17.50);
     }
 }
