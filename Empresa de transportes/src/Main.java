eac05850ac33628f243cbfd1412739a3e7c06a2b
WalterFox <john.cobacango@epn.edu.ec>
2024-06-14 12:18:14 -0500

    Creacion de objetos

--- Empresa de transportes/src/Main.java
+++ Empresa de transportes/src/Main.java
@@ -4,9 +4,19 @@ import Rutas.VIP;
 import Rutas.Normal;
 public class Main {
     public static void main(String[] args) {
+        Pasajero cliente1= new Pasajero(" Walter","Cobacango","Ponceano","0979376396",18);
+        Pasajero cliente2= new Pasajero("Kevin","Mendoza","Vicentina","0984168469",34);
+        Pasajero cliente3= new Pasajero("Richard","Acero","Zabala","0999999956",56);
+        Pasajero cliente4= new Pasajero("Martha","Cobacango","Cancelen","09867543929",30);
+        Pasajero cliente5= new Pasajero("Azucena","Acero","Cayambe","0995122669",53);
+
         Ruta ruta1 = new Ruta("Quito_Guayaquil", 20);
         Ruta ruta2 = new Ruta("Quito-Tulcán", 17.50);
         Ruta ruta3 = new Ruta("Quito-Puyo", 15);
         Ruta ruta4 = new Ruta("Quito-Riobamba", 17.50);
     }
+
+    // Valores nulos
+
+    
 }
